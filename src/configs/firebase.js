acf2e55a99f507d8ca810a52950b88618885f850
Norman Kita <kitanorman1@gmail.com>
2025-04-08 12:15:01 +0300

    First commit

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjX1ojaPxXqEbG1cwUo5m5Ibnsv3bMWag",
+  authDomain: "mzumbe-academic-portal.firebaseapp.com",
+  projectId: "mzumbe-academic-portal",
+  storageBucket: "mzumbe-academic-portal.firebasestorage.app",
+  messagingSenderId: "946292180292",
+  appId: "1:946292180292:web:a40a3859612733fecf8311",
+  measurementId: "G-32J5904Z3S"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// const analytics = getAnalytics(app);
