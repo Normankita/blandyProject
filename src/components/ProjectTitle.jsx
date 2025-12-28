629808c50ff9cb29feb8b1a520d61f0cf410240c
Norman Leonard kita <kitanorman1@gmail.com>
2025-12-28 15:08:20 +0300

    Logo

--- src/components/ProjectTitle.jsx
+++ src/components/ProjectTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectTitle = () => {
     return (
         <a href="#" className="flex flex-col items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <span><img src="/sitelogo.png" alt="" /></span>
+            <span className='flex flex-col items-center w-full'><img src="/car_garage.svg" alt="Garage LOgo" className='h-30 w-30' /></span>
             <span className="uppercase flex flex-col items-center overflow-auto text-center"><p>GARAGE MANAGEMENT SYSTEM</p></span>
         </a>
     );
